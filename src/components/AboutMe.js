72d823340c42f22bbe44f0fa43b51220a2288482
Torg777 <torg77007@gmail.com>
2024-11-27 23:28:26 +0400

    update info

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,18 +4,23 @@ function AboutMe() {
     return (
         <div className='AboutMe'>
             <h2 class="w3-text-grey w3-padding-16"><i class="fa fa-certificate fa-fw w3-margin-right w3-xxlarge w3-text-teal"></i>ABOUT ME</h2>
-            <p>Hello!</p>
-            <p>I am 23 years old. I completed JavaScript courses and am currently learning React. I have a good knowledge of HTML and CSS and experience with Bootstrap. I enjoy coding and solving challenging problems, and I am always eager to learn new things. Although I do not have official work experience yet, I have created several projects that showcase my skills. You can view them at the following links:</p>
+            <p>Hi!</p>
+            <p>I am 23 years old and passionate about web development. I have completed several courses in JavaScript and am currently deepening my knowledge of React. I have a strong foundation in HTML and CSS, and I am learning tools like Redux and Redux Toolkit for state management in applications.
+                I enjoy coding and solving challenging problems, and I am always eager to learn new technologies and improve my skills. Although I don't have official work experience yet, I have created several projects that showcase my abilities, and you can explore them through the following links:
+            </p>
 
             <ul>
                 <li>
-                    <a target="_blank" rel="noopener noreferrer" href='https://torg777.github.io/tic-tac-toe/'>Tic-tac-toe</a>
+                    <a target="_blank" rel="noopener noreferrer" href='https://torgompoghosyan.github.io/tic-tac-toe/'>Tic-tac-toe</a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noopener noreferrer" href='https://torg777.github.io/country-game/'>Country Game</a>
+                    <a target="_blank" rel="noopener noreferrer" href='https://torgompoghosyan.github.io/country-game/'>Country Game</a>
                 </li>
                 <li>
-                    <a target="_blank" rel="noopener noreferrer" href='https://torg777.github.io/calculator-js/'>Calculator Js</a>
+                    <a target="_blank" rel="noopener noreferrer" href='https://torgompoghosyan.github.io/calculator-js/'>Calculator Js</a>
+                </li>
+                <li>
+                    <a target="_blank" rel="noopener noreferrer" href='https://torgompoghosyan.github.io/translater/'>React translater</a>
                 </li>
             </ul>
 
